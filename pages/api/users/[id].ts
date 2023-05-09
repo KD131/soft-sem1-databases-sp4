568e8727bc0eca9d9257d3212e1db5c2f3799b43
KD131 <71063280+KD131@users.noreply.github.com>
2023-05-09 15:18:17 +0200

    4 spaces indentation

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -2,10 +2,10 @@ import redis from "@/lib/redis";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
-  const { id } = req.query;
-  const user = await redis.hgetall(`user:${id}`);
-  res.status(200).json(user)
+    const { id } = req.query;
+    const user = await redis.hgetall(`user:${id}`);
+    res.status(200).json(user)
 }
