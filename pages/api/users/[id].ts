d885c4eb0ff66c2c6216f5216840c70b66dd5e0c
KD131 <71063280+KD131@users.noreply.github.com>
2023-05-14 20:41:50 +0200

    gets nested user data

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -1,11 +1,34 @@
 import redis from "@/lib/redis";
 import { NextApiRequest, NextApiResponse } from "next";
 
+export async function postUser(id: string, data: any) {
+    redis.hset(`user:${id}`, data);
+    return data;
+}
+
+export async function getUser(id: string) {
+    const userCall = redis.hgetall(`user:${id}`) as Promise<Record<string, any>>;
+    const gamesCall = redis.smembers(`user:${id}:games`);
+    const [user, games] = await Promise.all([userCall, gamesCall]);
+    user.games = games;
+    return user;
+}
+
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     const { id } = req.query;
-    const user = await redis.hgetall(`user:${id}`);
-    res.status(200).json(user)
+    let user = null;
+
+    if (req.method === "GET") {
+        user = await getUser(id as string);
+    }
+    else if (req.method === "POST") {
+        user = await postUser(id as string, req.body);
+
+
+    }
+
+    res.status(200).json(user);
 }
